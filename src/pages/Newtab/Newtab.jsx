d3cd8da8da6e6a76e36dacd145bd7d2388518cf7
Yash Kumar Verma <yk.verma2000@gmail.com>
2021-03-28 20:26:47 +0530

    feat(signup): add register feature

--- src/pages/Newtab/Newtab.jsx
+++ src/pages/Newtab/Newtab.jsx
@@ -1,7 +1,68 @@
 import React from 'react';
 import './Newtab.css';
 import './Newtab.scss';
-import { LogInScript } from './script';
+import { LogInScript, SignUpScript } from './script';
+
+class SignUpForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+    };
+  }
+
+  /** function to update changes to state */
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <section className="text-gray-700 body-font">
+        <div className="container px-8 pt-48 pb-24 mx-auto lg:px-4">
+          <div className="flex flex-col w-full p-8 mx-auto mt-10 border rounded-lg lg:w-2/6 md:w-1/2 md:ml-auto md:mt-0">
+            <h3 class="mb-6 text-4xl font-semibold tracking-tighter text-black sm:text-5xl title-font">
+              <br className="md:hidden" />
+              <span className="text-green-800">Koala</span> - Borate
+            </h3>
+            <form onSubmit={this.handleSubmit}>
+              <div className="relative ">
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="your awesome email"
+                  onChange={this.handleChange}
+                  className="w-full px-4 py-2 mb-4 text-black transition duration-500 ease-in-out transform bg-gray-100 border-transparent rounded-lg mr-4text-base focus:border-gray-500 focus:bg-white focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2"
+                />
+              </div>
+              <div className="relative ">
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  placeholder="your secret password"
+                  onChange={this.handleChange}
+                  className="w-full px-4 py-2 mb-4 text-black transition duration-500 ease-in-out transform bg-gray-100 border-transparent rounded-lg mr-4text-base focus:border-gray-500 focus:bg-white focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2"
+                />
+              </div>
+              <button className="px-8 py-2 font-semibold text-white transition duration-500 ease-in-out transform bg-black rounded-lg hover:bg-gray-800 hover:to-black focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
+                Log In
+              </button>
+              <p className="mx-auto mt-3 text-xs text-gray-500">
+                HooHacks Rocks !
+              </p>
+            </form>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
 
 class LoginForm extends React.Component {
   constructor() {
@@ -10,6 +71,11 @@ class LoginForm extends React.Component {
       email: '',
       password: '',
       success: localStorage.getItem('token') !== null ? true : false,
+      signUpFirstName: '',
+      signUpLastName: '',
+      signUpEmail: '',
+      signUpPassword: '',
+      active: 'login',
     };
   }
 
@@ -20,7 +86,7 @@ class LoginForm extends React.Component {
   };
 
   /**  function to submit the form and perform login mechanism */
-  handleSubmit = (event) => {
+  handleLoginSubmit = (event) => {
     event.preventDefault();
 
     /** login into script  */
@@ -32,6 +98,34 @@ class LoginForm extends React.Component {
       .catch((e) => {
         this.setState({ success: false });
       });
+  };
+
+  /**  function to submit the form and perform login mechanism */
+  handleSignUpSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+    /** login into script  */
+    const { firstName, lastName, email, password } = this.state;
+    SignUpScript(firstName, lastName, email, password)
+      .then((resp) => {
+        this.navigateToLogIn();
+      })
+      .catch((e) => {
+        this.setState({ success: false });
+      });
+  };
+
+  navigateToSignUp = () => {
+    this.setState({
+      active: 'signup',
+    });
+    console.log(this.state);
+  };
+
+  navigateToLogIn = () => {
+    this.setState({
+      active: 'login',
+    });
   };
 
   logout = () => {
@@ -88,42 +182,115 @@ class LoginForm extends React.Component {
           </div>
         ) : (
           <section className="text-gray-700 body-font">
-            <div className="container px-8 pt-48 pb-24 mx-auto lg:px-4">
-              <div className="flex flex-col w-full p-8 mx-auto mt-10 border rounded-lg lg:w-2/6 md:w-1/2 md:ml-auto md:mt-0">
-                <h3 class="mb-6 text-4xl font-semibold tracking-tighter text-black sm:text-5xl title-font">
-                  <br className="md:hidden" />
-                  <span className="text-green-800">Koala</span> - Borate
-                </h3>
-                <form onSubmit={this.handleSubmit}>
-                  <div className="relative ">
-                    <input
-                      type="email"
-                      id="email"
-                      name="email"
-                      placeholder="your awesome email"
-                      onChange={this.handleChange}
-                      className="w-full px-4 py-2 mb-4 text-black transition duration-500 ease-in-out transform bg-gray-100 border-transparent rounded-lg mr-4text-base focus:border-gray-500 focus:bg-white focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2"
-                    />
-                  </div>
-                  <div className="relative ">
-                    <input
-                      type="password"
-                      id="password"
-                      name="password"
-                      placeholder="your secret password"
-                      onChange={this.handleChange}
-                      className="w-full px-4 py-2 mb-4 text-black transition duration-500 ease-in-out transform bg-gray-100 border-transparent rounded-lg mr-4text-base focus:border-gray-500 focus:bg-white focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2"
-                    />
-                  </div>
-                  <button className="px-8 py-2 font-semibold text-white transition duration-500 ease-in-out transform bg-black rounded-lg hover:bg-gray-800 hover:to-black focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
-                    Log In
-                  </button>
-                  <p className="mx-auto mt-3 text-xs text-gray-500">
-                    HooHacks Rocks !
-                  </p>
-                </form>
+            {this.state.active === 'login' ? (
+              <div className="container px-8 pt-48 pb-24 mx-auto lg:px-4">
+                <div className="flex flex-col w-full p-8 mx-auto mt-10 border rounded-lg lg:w-2/6 md:w-1/2 md:ml-auto md:mt-0">
+                  <h3 class="mb-6 text-4xl font-semibold tracking-tighter text-black sm:text-5xl title-font">
+                    <br className="md:hidden" />
+                    <span className="text-green-800">Koala</span> - Borate
+                  </h3>
+                  <form onSubmit={this.handleLoginSubmit}>
+                    <div className="relative ">
+                      <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="your awesome email"
+                        onChange={this.handleChange}
+                        className="w-full px-4 py-2 mb-4 text-black transition duration-500 ease-in-out transform bg-gray-100 border-transparent rounded-lg mr-4text-base focus:border-gray-500 focus:bg-white focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2"
+                      />
+                    </div>
+                    <div className="relative ">
+                      <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        placeholder="your secret password"
+                        onChange={this.handleChange}
+                        className="w-full px-4 py-2 mb-4 text-black transition duration-500 ease-in-out transform bg-gray-100 border-transparent rounded-lg mr-4text-base focus:border-gray-500 focus:bg-white focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2"
+                      />
+                    </div>
+                    <button className="px-8 py-2 font-semibold text-white transition duration-500 ease-in-out transform bg-black rounded-lg hover:bg-gray-800 hover:to-black focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
+                      Log In
+                    </button>
+                    <p className="mx-auto mt-3 text-xs text-gray-500">
+                      HooHacks Rocks ! Not yet registered,{' '}
+                      <span
+                        className="underline text-green-400"
+                        onClick={this.navigateToSignUp.bind(this)}
+                      >
+                        register here
+                      </span>{' '}
+                      !
+                    </p>
+                  </form>
+                </div>
               </div>
-            </div>
+            ) : (
+              <div className="container px-8 pt-48 pb-24 mx-auto lg:px-4">
+                <div className="flex flex-col w-full p-8 mx-auto mt-10 border rounded-lg lg:w-2/6 md:w-1/2 md:ml-auto md:mt-0">
+                  <h3 class="mb-6 text-4xl font-semibold tracking-tighter text-black sm:text-5xl title-font">
+                    <br className="md:hidden" />
+                    <span className="text-green-800">Koala</span> - Borate
+                  </h3>
+                  <form onSubmit={this.handleSignUpSubmit}>
+                    <div className="relative ">
+                      <input
+                        type="text"
+                        id="signUpFirstName"
+                        name="signUpFirstName"
+                        placeholder="your first name"
+                        onChange={this.handleChange}
+                        className="w-full px-4 py-2 mb-4 text-black transition duration-500 ease-in-out transform bg-gray-100 border-transparent rounded-lg mr-4text-base focus:border-gray-500 focus:bg-white focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2"
+                      />
+                    </div>
+                    <div className="relative ">
+                      <input
+                        type="text"
+                        id="signUpLastName"
+                        name="signUpLastName"
+                        placeholder="your last name"
+                        onChange={this.handleChange}
+                        className="w-full px-4 py-2 mb-4 text-black transition duration-500 ease-in-out transform bg-gray-100 border-transparent rounded-lg mr-4text-base focus:border-gray-500 focus:bg-white focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2"
+                      />
+                    </div>
+                    <div className="relative ">
+                      <input
+                        type="email"
+                        id="signUpEmail"
+                        name="signUpEmail"
+                        placeholder="your email"
+                        onChange={this.handleChange}
+                        className="w-full px-4 py-2 mb-4 text-black transition duration-500 ease-in-out transform bg-gray-100 border-transparent rounded-lg mr-4text-base focus:border-gray-500 focus:bg-white focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2"
+                      />
+                    </div>
+                    <div className="relative ">
+                      <input
+                        type="password"
+                        id="signUpPassword"
+                        name="signUpPassword"
+                        placeholder="your secure password"
+                        onChange={this.handleChange}
+                        className="w-full px-4 py-2 mb-4 text-black transition duration-500 ease-in-out transform bg-gray-100 border-transparent rounded-lg mr-4text-base focus:border-gray-500 focus:bg-white focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2"
+                      />
+                    </div>
+                    <button className="px-8 py-2 font-semibold text-white transition duration-500 ease-in-out transform bg-black rounded-lg hover:bg-gray-800 hover:to-black focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
+                      Register
+                    </button>
+                    <p className="mx-auto mt-3 text-xs text-gray-500">
+                      HooHacks Rocks ! Already a member?,{' '}
+                      <span
+                        className="underline text-green-400"
+                        onClick={this.navigateToLogIn.bind(this)}
+                      >
+                        login here
+                      </span>{' '}
+                      !
+                    </p>
+                  </form>
+                </div>
+              </div>
+            )}
           </section>
         )}
       </div>
--- src/pages/Newtab/script.js
+++ src/pages/Newtab/script.js
@@ -2,20 +2,24 @@ const axios = require('axios').default;
 const { CONFIG } = require('../../config');
 
 /** to signup into server */
-// export const SignUpScript = async (email, password) =>
-//   new Promise((resolve, reject) => {
-//     console.log('starting login process');
-//     axios
-//       .post(`${CONFIG.URL}/user`, { email, password })
-//       .then(({ data }) => {
-//         console.log(data);
-//         localStorage.setItem('token', data.payload);
-//         resolve({ error: false });
-//       })
-//       .catch((error) => {
-//         reject(error.response.data);
-//       });
-//   });
+export const SignUpScript = async (firstName, lastName, email, password) =>
+  new Promise((resolve, reject) => {
+    console.log('starting login process');
+    axios
+      .post(`${CONFIG.URL}/user`, {
+        firstName,
+        lastName,
+        email,
+        password,
+        role: 'student',
+      })
+      .then(({ data }) => {
+        resolve({ ops: true });
+      })
+      .catch((error) => {
+        reject(error.response.data);
+      });
+  });
 
 /** to login into server */
 export const LogInScript = async (email, password) =>
